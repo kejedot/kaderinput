7386bdb3cbb478808c1de124c8b23c8b8e2544a2
kejedot <kegedung@gmail.com>
2026-09-15 16:28:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const API =
+"https://script.google.com/macros/s/AKfycbzDoyMuJjoIYO3KO3e1KEo8whcj7Hyv_-QZu18gtz4PAu6cR3ZxD-_exYVzcvJ2JetByw/exec";
+
+
+function kirim(){
+
+let data={
+
+nama:
+document.getElementById("nama").value,
+
+hp:
+document.getElementById("hp").value,
+
+lokasi:
+document.getElementById("lokasi").value,
+
+keterangan:
+document.getElementById("ket").value
+
+};
+
+
+fetch(API,{
+
+method:"POST",
+
+body:JSON.stringify(data)
+
+})
+
+.then(res=>res.json())
+
+.then(result=>{
+
+document.getElementById("hasil")
+.innerHTML="Data tersimpan";
+
+});
+
+}
