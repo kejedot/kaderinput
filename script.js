6f0e0df987ebdfa11184ccbc5808d95089ff4625
kejedot <kegedung@gmail.com>
2026-09-15 17:10:21 +0700

    script.js

--- script.js
+++ script.js
@@ -3,6 +3,80 @@ alert("Javascript aktif");
 const API =
 "https://script.google.com/macros/s/AKfycbzDoyMuJjoIYO3KO3e1KEo8whcj7Hyv_-QZu18gtz4PAu6cR3ZxD-_exYVzcvJ2JetByw/exec";
 
+window.onload = function(){
+
+    ambilGPS();
+
+};
+
+
+function ambilGPS(){
+
+    if(navigator.geolocation){
+
+
+        navigator.geolocation.getCurrentPosition(
+
+        function(position){
+
+
+            let latitude =
+            position.coords.latitude;
+
+
+            let longitude =
+            position.coords.longitude;
+
+
+            document.getElementById("latitude").value =
+            latitude;
+
+
+            document.getElementById("longitude").value =
+            longitude;
+
+
+            document.getElementById("gpsStatus").innerHTML =
+            "Lokasi berhasil didapat";
+
+
+            console.log(
+              "GPS:",
+              latitude,
+              longitude
+            );
+
+
+        },
+
+
+        function(error){
+
+            document.getElementById("gpsStatus").innerHTML =
+            "GPS gagal diambil";
+
+
+            console.log(error);
+
+        },
+
+
+        {
+            enableHighAccuracy:true,
+            timeout:10000,
+            maximumAge:0
+        }
+
+        );
+
+
+    }else{
+
+        alert("GPS tidak didukung browser");
+
+    }
+
+}
 
 function kirim(){
 
@@ -35,5 +109,33 @@ function kirim(){
         document.getElementById("hasil").innerHTML =
         "Terjadi error";
     });
+
+}
+
+function kirim(){
+
+
+let lat =
+document.getElementById("latitude").value;
+
+
+let lon =
+document.getElementById("longitude").value;
+
+
+if(lat=="" || lon==""){
+
+alert("Lokasi belum tersedia, tunggu GPS aktif");
+
+return;
+
+let accuracy =
+position.coords.accuracy;
+
+}
+
+
+// lanjut kirim data
+
 
 }
